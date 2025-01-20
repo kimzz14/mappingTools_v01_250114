e6861c3e8c63ef1174c9537a9e3ef98a32dfb0e7
kimzz14 <kimzz1004@gmail.com>
2025-01-20 14:19:57 +0900

    bug

--- pipe/samtools-index.sh
+++ pipe/samtools-index.sh
@@ -15,6 +15,6 @@ fi
 
 samtools index \
     --threads ${threadN} \
-    -c  result/${readID}.sorted.bam \
-    1>  result/${readID}.sorted.bam.csi.log \
-    2>  result/${readID}.sorted.bam.csi.err
+    -c  result/${readID}.bam \
+    1>  result/${readID}.bam.csi.log \
+    2>  result/${readID}.bam.csi.err
